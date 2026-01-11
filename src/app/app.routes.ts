9fe3e09e93b579598db31d5e9b25b6f684934291
mariepcc <maria.pacocha358@gmail.com>
2026-01-11 16:25:37 +0100

    feat: add basic app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { RegisterComponent } from "./pages/auth/register/register.component";
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { CountrySelectorComponent } from "./pages/selector/country-selector/country-selector.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: MainPageComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+  {
+    path: "country-selector",
+    component: CountrySelectorComponent,
+  },
+];
