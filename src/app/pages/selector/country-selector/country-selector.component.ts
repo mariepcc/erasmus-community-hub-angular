5159073f2da1c10b465db9191b83c3af260deb1b
mariepcc <maria.pacocha358@gmail.com>
2026-01-11 18:22:49 +0100

    feat: install GSAP and implement simple animation

--- src/app/pages/selector/country-selector/country-selector.component.ts
+++ src/app/pages/selector/country-selector/country-selector.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
 
+gsap.registerPlugin();
 @Component({
   selector: 'app-country-selector',
   imports: [],
   templateUrl: './country-selector.component.html',
-  styleUrl: './country-selector.component.css'
+  styleUrl: './country-selector.component.css',
 })
-export class CountrySelectorComponent {
+export class CountrySelectorComponent implements AfterViewInit {
+  @ViewChild('box') box!: ElementRef;
 
+  ngAfterViewInit() {
+    console.log('Target:', this.box.nativeElement);
+    gsap.to(this.box.nativeElement, { duration: 1, x: 100, opacity: 1 });
+  }
 }
