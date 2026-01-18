f4850dfbb0d7e4ffae0c7370827b7c10e5f3721d
mariepcc <maria.pacocha358@gmail.com>
2026-01-18 01:42:56 +0100

    fix: make coutry selector carousel responsive

--- src/app/pages/selector/country-selector/country-selector.component.ts
+++ src/app/pages/selector/country-selector/country-selector.component.ts
@@ -12,7 +12,6 @@ import {
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 import { gsap } from 'gsap';
-import { Draggable, ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-country-selector',
@@ -23,24 +22,17 @@ import { Draggable, ScrollTrigger } from 'gsap/all';
 })
 export class CountrySelectorComponent implements AfterViewInit, OnDestroy {
   private router = inject(Router);
+  private platformId = inject(PLATFORM_ID);
 
   @ViewChild('galleryWrapper') galleryWrapper!: ElementRef;
-  @ViewChild('cardsList') cardsList!: ElementRef;
   @ViewChildren('cardItem') cardItems!: QueryList<ElementRef>;
 
   public selectedCountries: string[] = [];
-
-  private platformId = inject(PLATFORM_ID);
   private ctx?: gsap.Context;
-
-  private moveToOffset?: (offset: number) => void;
-  private currentOffset = 0;
   private spacing = 0.1;
-  private scrollEndCallback?: gsap.Callback;
-
-  constructor() {
-    gsap.registerPlugin(ScrollTrigger, Draggable);
-  }
+  private seamlessLoop?: gsap.core.Timeline;
+  private scrub?: gsap.core.Tween;
+  private playhead = { offset: 0 };
 
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -52,24 +44,29 @@ export class CountrySelectorComponent implements AfterViewInit, OnDestroy {
     if (this.ctx) {
       this.ctx.revert();
     }
-
-    ScrollTrigger.getAll().forEach((t) => t.kill());
-
-    if (this.scrollEndCallback) {
-      ScrollTrigger.removeEventListener('scrollEnd', this.scrollEndCallback);
-    }
   }
 
   next() {
-    if (this.moveToOffset) {
-      this.moveToOffset(this.currentOffset + this.spacing);
-    }
+    this.scrubTo(this.playhead.offset + this.spacing);
   }
 
   prev() {
-    if (this.moveToOffset) {
-      this.moveToOffset(this.currentOffset - this.spacing);
-    }
+    this.scrubTo(this.playhead.offset - this.spacing);
+  }
+
+  private scrubTo(offset: number) {
+    const snap = gsap.utils.snap(this.spacing);
+    this.scrub?.kill();
+    this.scrub = gsap.to(this.playhead, {
+      offset: snap(offset),
+      duration: 0.5,
+      ease: 'power3',
+      onUpdate: () => {
+        const totalTime = this.seamlessLoop?.duration() || 0;
+        const wrap = gsap.utils.wrap(0, totalTime);
+        this.seamlessLoop?.time(wrap(this.playhead.offset));
+      },
+    });
   }
 
   toggleCountry(countryName: string) {
@@ -86,7 +83,6 @@ export class CountrySelectorComponent implements AfterViewInit, OnDestroy {
   }
 
   confirmSelection() {
-    console.log('Wybrane kraje:', this.selectedCountries);
     this.router.navigate(['/city-selector'], {
       queryParams: { countries: this.selectedCountries },
     });
@@ -95,13 +91,6 @@ export class CountrySelectorComponent implements AfterViewInit, OnDestroy {
   private initSeamlessLoop() {
     this.ctx = gsap.context(() => {
       const cards = this.cardItems.map((el) => el.nativeElement);
-
-      this.spacing = 0.1;
-
-      let iteration = 0;
-      const spacing = this.spacing;
-      const snapTime = gsap.utils.snap(spacing);
-
       gsap.set(cards, { xPercent: 400, opacity: 0, scale: 0 });
 
       const animateFunc = (element: HTMLElement) => {
@@ -128,90 +117,12 @@ export class CountrySelectorComponent implements AfterViewInit, OnDestroy {
         return tl;
       };
 
-      const seamlessLoop = this.buildSeamlessLoop(cards, spacing, animateFunc);
-      const playhead = { offset: 0 };
-      const wrapTime = gsap.utils.wrap(0, seamlessLoop.duration());
-
-      const scrub = gsap.to(playhead, {
-        offset: 0,
-        onUpdate: () => {
-          seamlessLoop.time(wrapTime(playhead.offset));
-        },
-        duration: 0.5,
-        ease: 'power3',
-        paused: true,
-      });
-
-      const trigger = ScrollTrigger.create({
-        start: 0,
-
-        onUpdate: (self) => {
-          const scroll = self.scroll();
-
-          if (scroll > self.end - 1) {
-            wrap(1, 2);
-          } else if (scroll < 1 && self.direction < 0) {
-            wrap(-1, self.end - 2);
-          } else {
-            const newOffset =
-              (iteration + self.progress) * seamlessLoop.duration();
-            scrub.vars['offset'] = newOffset;
-            scrub.invalidate().restart();
-            this.currentOffset = newOffset;
-          }
-        },
-        end: '+=3000',
-        pin: this.galleryWrapper.nativeElement,
-      });
-
-      const getSafeScroll = (progress: number) => {
-        return gsap.utils.clamp(
-          1,
-          trigger.end - 1,
-          gsap.utils.wrap(0, 1, progress) * trigger.end,
-        );
-      };
-
-      const wrap = (iterationDelta: number, scrollTo: number) => {
-        iteration += iterationDelta;
-        trigger.scroll(scrollTo);
-        trigger.update();
-      };
-
-      this.scrollEndCallback = () => scrollToOffset(scrub.vars['offset']);
-      ScrollTrigger.addEventListener('scrollEnd', this.scrollEndCallback);
-
-      const scrollToOffset = (offset: number) => {
-        const snappedTime = snapTime(offset);
-        const progress =
-          (snappedTime - seamlessLoop.duration() * iteration) /
-          seamlessLoop.duration();
-        const scroll = getSafeScroll(progress);
-
-        if (progress >= 1 || progress < 0) {
-          return wrap(Math.floor(progress), scroll);
-        }
-        trigger.scroll(scroll);
-      };
-
-      this.moveToOffset = scrollToOffset;
-
-      Draggable.create('.drag-proxy', {
-        type: 'x',
-        trigger: this.cardsList.nativeElement,
-        onPress() {
-          this['startOffset'] = scrub.vars['offset'];
-        },
-        onDrag() {
-          const newOffset =
-            this['startOffset'] + (this['startX'] - this['x']) * 0.001;
-          scrub.vars['offset'] = newOffset;
-          scrub.invalidate().restart();
-        },
-        onDragEnd() {
-          scrollToOffset(scrub.vars['offset'] as number);
-        },
-      });
+      this.seamlessLoop = this.buildSeamlessLoop(
+        cards,
+        this.spacing,
+        animateFunc,
+      );
+      this.playhead.offset = this.seamlessLoop.duration() * 100;
     }, this.galleryWrapper);
   }
 
@@ -235,11 +146,9 @@ export class CountrySelectorComponent implements AfterViewInit, OnDestroy {
     });
 
     let l = items.length + overlap * 2;
-    let time, i, index;
-
-    for (i = 0; i < l; i++) {
-      index = i % items.length;
-      time = i * spacing;
+    for (let i = 0; i < l; i++) {
+      let index = i % items.length;
+      let time = i * spacing;
       rawSequence.add(animateFunc(items[index]), time);
     }
 
